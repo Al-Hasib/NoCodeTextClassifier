1235d5ef38147fad1b8f5de9e50341f4e4e20cc4
Md Abdullah  Al Hasib <md.abdullah@crtvai.com>
2025-02-02 10:09:58 +0600

    update

--- NoCodeTextClassifier/preprocessing.py
+++ NoCodeTextClassifier/preprocessing.py
@@ -99,7 +99,8 @@ class process:
         df['clean_text'] = self.clean_text(df)
         return df
     
-    def split_data(self, X, y):
+    @staticmethod
+    def split_data( X, y):
         from sklearn.model_selection import train_test_split
         X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
         return X_train, X_test, y_train, y_test
--- app.py
+++ app.py
@@ -25,65 +25,94 @@ if train_data is not None and test_data is not None:
     text_data = st.sidebar.selectbox("Choose the text column:", columns)
     target = st.sidebar.selectbox("Choose the target column:", columns)
 
+    info = Informations(train_df, text_data, target)
+    train_df['clean_text'] = info.clean_text()
+    train_df['text_length'] = info.text_length()
+    train_df['target'] = info.label_encoder()
+
+    # test_info = Informations(test_df, text_data, target)
+    # test_df['clean_text'] = test_info.clean_text()
+    # test_df['text_length'] = test_info.text_length()
+
 
 
     
 
 if section=="Data Analysis":
-    st.subheader("Get Insights from the Data")
-    info = Informations(train_df, text_data, target)
-    st.write("Data Shape:", info.shape())
-    st.write("Class Imbalance:", info.class_imbalanced())
-    st.write("Missing Values:", info.missing_values())
+    try:
+        st.subheader("Get Insights from the Data")
+        
+        st.write("Data Shape:", info.shape())
+        st.write("Class Imbalance:", info.class_imbalanced())
+        st.write("Missing Values:", info.missing_values())
 
-    train_df['clean_text'] = info.clean_text()
-    train_df['text_length'] = info.text_length()
-    train_df['target'] = info.label_encoder()
-
-    st.write(train_df.head(3))
-    st.markdown("**Text Length Analysis**")
-    st.write(info.analysis_text_length('text_length'))
-    st.write("Correlation between Text Length and Target:", info.correlation('text_length'))
+        st.write(train_df.head(3))
+        st.markdown("**Text Length Analysis**")
+        st.write(info.analysis_text_length('text_length'))
+        st.write("Correlation between Text Length and Target:", info.correlation('text_length'))
 
 
-    st.subheader("Visualizations")
-    vis = Visualizations(train_df, text_data, target)
-    vis.class_distribution()
-    vis.text_length_distribution()
+        st.subheader("Visualizations")
+        vis = Visualizations(train_df, text_data, target)
+        vis.class_distribution()
+        vis.text_length_distribution()
+
+    except Exception as e:
+        st.write("Please upload the data to get the insights")
+        
 
 if section=="Train Model":
-    st.subheader("Train a Model")
+    try:
+        st.subheader("Train a Model")
 
-    info = Informations(test_df, text_data, target)
-    test_df['clean_text'] = info.clean_text()
-    test_df['text_length'] = info.text_length()
-    test_df['target'] = info.label_encoder()
-    # Create two columns
-    col1, col2 = st.columns(2)
+        
+        # Create two columns
+        col1, col2 = st.columns(2)
 
-    with col1:
-        model = st.radio("Choose the Model", ["Logistic Regression","Decision Tree", 
-                         "Random Forest", "Linear SVC", "SVC",
-                         "Multinomial Naive Bayes", "Gaussian Naive Bayes"])
-    with col2:
-        vectorizer = st.radio("Choose Vectorizer", ["Tfidf Vectorizer", "Count Vectorizer"])
+        with col1:
+            model = st.radio("Choose the Model", ["Logistic Regression","Decision Tree", 
+                            "Random Forest", "Linear SVC", "SVC",
+                            "Multinomial Naive Bayes", "Gaussian Naive Bayes"])
+        with col2:
+            vectorizer = st.radio("Choose Vectorizer", ["Tfidf Vectorizer", "Count Vectorizer"])
 
-    if vectorizer=="Tfidf Vectorizer":
-        vectorizer = TfidfVectorizer(max_features=10000)
-    else:
-        vectorizer = CountVectorizer(max_features=10000)
-    
-    X_train = vectorizer.fit_transform(train_df['clean_text'])
-    y_train = train_df['target']
+        if vectorizer=="Tfidf Vectorizer":
+            vectorizer = TfidfVectorizer(max_features=10000)
+        else:
+            vectorizer = CountVectorizer(max_features=10000)
+        
+        st.write(train_df.head(3))
+        X = vectorizer.fit_transform(train_df['clean_text'])
+        y = train_df['target']
+        X_train, X_test, y_train, y_test = process.split_data(X, y)
+        st.write(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+        if st.button("Start Training"):
+            models = Models(X_train=X_train,X_test = X_test, y_train = y_train, y_test = y_test)
+            
+            if model=="Logistic Regression":
+                models.LogisticRegression()
+            elif model=="Decision Tree":
+                models.DecisionTree()
+            elif model=="Linear SVC":
+                models.LinearSVC()
+            elif model=="SVC":
+                models.SVC()
+            elif model=="Multinomial Naive Bayes":
+                models.MultinomialNB()
+            elif model=="Random Forest":
+                models.RandomForestClassifier()
+            else:
+                models.GaussianNB()
 
-    X_test = vectorizer.fit_transform(test_df['clean_text'])
-    y_test = test_df['target']
-
-    models = Models(X_train=X_train,X_test = X_test, y_train = y_train, y_test = y_test)
-
-    models.DecisionTree()
+    except Exception as e:
+        st.write("Please upload the data to train the model")
     
 
 if section=="Predictions":
-    st.subheader("Perform Predictions on the Test Data")
+    try:
+        st.subheader("Perform Predictions on the Test Data")
+        text = st.text_area("Enter the text to classify")
+    
+    except Exception as e:
+        st.write("Please Train the Model to perform the predictions")
 
